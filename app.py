48559974db6d788e311e23a718ee2a344b283676
FVCproductions <hello@fvcproductions.com>
2016-11-09 12:54:39 -0500

    trying
    Signed-off-by: FVCproductions <hello@fvcproductions.com>

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ import time
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return render_template('index.html', time=epochTime())
 
 def epochTime():
     return '<h1>' + str(int(time.time())) + '</h1>'
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
---
+++ time.wsgi
@@ -0,0 +1,4 @@
+import sys
+sys.path.insert(0, '/var/www/html/time')
+
+from time import app as application
